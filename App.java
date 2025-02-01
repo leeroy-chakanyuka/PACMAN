f138f8304d97226cbafb6d77aeb6f6caf3925f11
Leeroy Chakanyuka <168822625+leeroy-chakanyuka@users.noreply.github.com>
2025-02-01 12:35:16 +0200

    Update App.java - clean up

--- App.java
+++ App.java
@@ -8,19 +8,6 @@ public class App {
         int boardWidth = colCount * tileSize;
         int boardHeight = rowCount * tileSize;
 
-//        JFrame myFrame = new JFrame("Leeroy's Pac Man");
-//
-////        myFrame.setLocationRelativeTo(null); //pop up in the center
-//
-//        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //prevent memory leakage
-//        myFrame.setResizable(false); // not a flow layout, will not be responsive to dimension change
-//        myFrame.setSize(boardWidth, boardHeight);
-//
-//        PacMan myPaccy = new PacMan();
-//        myFrame.add(myPaccy);
-//        myFrame.pack(); // makes it fit all the components even without a specified size
-//        myFrame.setVisible(true); //display
-
         JFrame frame = new JFrame("Leeroy's PacMan");
         PacMan pacMan = new PacMan();
         ImageIcon myLogo = new ImageIcon("src/img.png");
